b443d3fe11c4a6d602cb1a427e71d66f7c557ed2
Marina <mv.nikolaeva1507@gmail.com>
2022-08-16 19:11:30 +0300

    add homework 4 Task 29

---
+++ hw4_task29/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 29: Напишите программу, которая 
+// 1. задаёт массив из 8 элементов и 
+// 2. выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+Console.Write("Введите количество элементов массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 0)
+{
+    Console.Write("Вы ввели недопустимое значение");
+    return;
+}
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+int[] result = GetArray(number);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]} -> [");
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+PrintArray(result);
