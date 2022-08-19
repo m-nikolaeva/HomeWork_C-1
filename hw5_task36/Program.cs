e5318e4dfcd5fff0a6c26572b43e16d8286ded66
Marina <mv.nikolaeva1507@gmail.com>
2022-08-19 19:36:46 +0300

    add homework 5 Task 36

---
+++ hw5_task36/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 36: 
+// 1. Задайте одномерный массив, заполненный случайными числами. 
+// 2. Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+int SumOddIndArray(int[] array)
+{
+    int sumodd = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sumodd += array[i];
+    }
+    return sumodd;
+;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+int[] arr = CreateArrayRndInt(5, -10, 20);
+int sumoddind = SumOddIndArray(arr);
+
+PrintArray(arr);
+Console.Write($" -> {sumoddind}");
