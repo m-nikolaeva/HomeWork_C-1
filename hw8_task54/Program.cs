3e6b898bf0742afc5cb8aa1bfc9bde43366e662f
Marina <mv.nikolaeva1507@gmail.com>
2022-08-30 18:51:36 +0300

    add HomeWork 8 task 54

---
+++ hw8_task54/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+Console.WriteLine("Введите число строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] mtx = CreateMatrixRndInt(m, n, 1, 9);
+
+Console.WriteLine("Исходная матрица:");
+PrintMatrix(mtx);
+
+Console.WriteLine("Модифицированная матрица:");
+SelectionSortRow(mtx);
+PrintMatrix(mtx);
+
+Console.WriteLine();
+
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void SelectionSortRow(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1)-1; j++)
+        {
+            for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+            {
+            if (matrix[i, col] < matrix[i, col + 1])
+            { 
+            int temporary = matrix[i, col];
+            matrix[i, col] = matrix[i, col + 1];
+            matrix[i, col + 1] = temporary;
+            }
+            }
+        }
+
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
