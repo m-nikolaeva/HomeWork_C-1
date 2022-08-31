b3da639f39dec5fb05c8054036a32edba09ea8f0
Marina <mv.nikolaeva1507@gmail.com>
2022-08-31 19:08:14 +0300

    add HomeWork 8 task 62

---
+++ hw8_task62/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+Console.Write("Введите размер массива -> ");
+int size1 = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] SpiralMatrix (int size)
+{
+int[,] spiral = new int[size, size];
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= size * size)
+{
+    spiral[i, j] = num;
+    if (i <= j + 1 && i + j < size - 1)
+        ++j;
+    else if (i < j && i + j >= size - 1)
+        ++i;
+    else if (i >= j && i + j > size - 1)
+        --j;
+    else
+        --i;
+    ++num;
+}
+return spiral;
+}
+
+
+int[,] spiralMatrix = SpiralMatrix (size1); 
+PrintMatrix(spiralMatrix);
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
